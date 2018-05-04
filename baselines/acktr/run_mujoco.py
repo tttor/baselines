78891c6e9254a21fee7e94fcd35f5b10f00338ad
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-05-04 15:37:58 +1000

    mbed train() to main()

--- baselines/acktr/run_mujoco.py
+++ baselines/acktr/run_mujoco.py
@@ -7,12 +7,19 @@ from baselines.acktr.acktr_cont import learn
 from baselines.acktr.policies import GaussianMlpPolicy
 from baselines.acktr.value_functions import NeuralNetValueFunction
 
-def train(env_id, num_timesteps, seed):
-    env = make_mujoco_env(env_id, seed)
+def main():
+    args = mujoco_arg_parser().parse_args()
+    logger.configure()
 
+    env = make_mujoco_env(args.env, args.seed)
+
+    ## train
     with tf.Session(config=tf.ConfigProto()):
         ob_dim = env.observation_space.shape[0]
         ac_dim = env.action_space.shape[0]
+        print('ob_dim= '+str(ob_dim))
+        print('ac_dim= '+str(ac_dim))
+
         with tf.variable_scope("vf"):
             vf = NeuralNetValueFunction(ob_dim, ac_dim)
         with tf.variable_scope("pi"):
@@ -21,14 +28,9 @@ def train(env_id, num_timesteps, seed):
         learn(env, policy=policy, vf=vf,
             gamma=0.99, lam=0.97, timesteps_per_batch=2500,
             desired_kl=0.002,
-            num_timesteps=num_timesteps, animate=False)
+            num_timesteps=args.num_timesteps, animate=False)
 
-        env.close()
-
-def main():
-    args = mujoco_arg_parser().parse_args()
-    logger.configure()
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
+    env.close()
 
 if __name__ == "__main__":
     main()
