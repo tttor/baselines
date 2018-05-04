2fe93d48226f5b0ff4cf479babc24d76ddf5eda2
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-05-05 05:23:17 +1000

    mv iter_idx

--- baselines/acktr/acktr_cont.py
+++ baselines/acktr/acktr_cont.py
@@ -78,19 +78,19 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, num_timesteps,
         assert (qr != None)
         enqueue_threads.extend( qr.create_threads(tf.get_default_session(), coord=coord, start=True) )
 
-    i = 0
+    iter_idx = 0
     timesteps_so_far = 0
     while True:
         if timesteps_so_far > num_timesteps:
             break
-        logger.log("********** Iteration %i ************"%i)
+        logger.log("********** Learning Iteration % i ************"%iter_idx)
 
         # Collect paths until we have enough timesteps
         timesteps_this_batch = 0
         paths = []
         while True:
             path = rollout(env, policy, max_pathlength,
-                           render=(len(paths)==0 and (i % 10 == 0) and animate),
+                           render=(len(paths)==0 and (iter_idx % 10 == 0) and animate),
                            obfilter=obfilter)
 
             paths.append(path)
@@ -147,7 +147,7 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, num_timesteps,
 
         if callback: callback()
         logger.dump_tabular()
-        i += 1
+        iter_idx += 1
 
     coord.request_stop()
     coord.join(enqueue_threads)
