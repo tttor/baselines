7bc5fde79daa2b167a50c1e20cf808f77fec4024
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-05-06 17:15:07 +1000

    mv rollout to run_one_episode

--- baselines/acktr/run_mujoco.py
+++ baselines/acktr/run_mujoco.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import tensorflow as tf
 
 from baselines import logger
@@ -30,6 +31,7 @@ def main():
 
         acktr_cont.learn(env,
                          policy=policy, vf=vf,
+                         rollout=run_one_episode,
                          gamma=0.99, lam=0.97,
                          batch_size=2500,# in nsteps
                          max_nsteps=args.num_timesteps,
@@ -37,6 +39,57 @@ def main():
                          animate=False)
 
     env.close()
+
+def run_one_episode(env, policy, max_pathlength, render=False, obfilter=None):
+    """
+    Simulate the env and policy for max_pathlength steps
+    """
+    ob = env.reset()
+    prev_ob = np.float32(np.zeros(ob.shape))
+    if obfilter: ob = obfilter(ob)
+    terminated = False
+    obs = []
+    acs = []
+    ac_dists = []
+    logps = []
+    rewards = []
+
+    for step_idx in range(max_pathlength):
+        ## get obs
+        state = np.concatenate([ob, prev_ob], -1)
+        obs.append(state)
+        prev_ob = np.copy(ob)
+
+        ## get action
+        ac, ac_dist, logp = policy.act(state)
+        acs.append(ac)
+        ac_dists.append(ac_dist)
+        logps.append(logp)
+
+        scaled_ac = env.action_space.low + (ac + 1.) * 0.5 * (env.action_space.high - env.action_space.low)
+        scaled_ac = np.clip(scaled_ac, env.action_space.low, env.action_space.high)
+
+        ## step
+        ob, rew, done, info = env.step(scaled_ac)
+
+        rewards.append(rew)
+        if obfilter:
+            ob = obfilter(ob)
+        if done:
+            terminated = True
+            break
+
+        if render:
+            print('--- step_idx= %i ---' % step_idx)
+            print('scaled_ac= %f, %f' % (scaled_ac[0], scaled_ac[1]))
+            print('rew(=dist+ctrl)= %f (=%f + %f)' % (rew,info['reward_dist'],info['reward_ctrl']))
+            print(str(ob))
+            env.render()
+            time.sleep(0.100)
+
+    return {"observation" : np.array(obs), "terminated" : terminated,
+            "reward" : np.array(rewards), "action" : np.array(acs),
+            "action_dist": np.array(ac_dists), "logp" : np.array(logps)}
 
 if __name__ == "__main__":
     main()
