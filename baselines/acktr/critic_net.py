36cd4ac2d10bb1e60b6dc025e6bdeca68f7fc386
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-05-08 18:54:58 +1000

    rm pylint: disable=W0613

--- baselines/acktr/critic_net.py
+++ baselines/acktr/critic_net.py
@@ -7,7 +7,7 @@ from baselines.acktr import kfac
 from baselines.acktr.utils import dense
 
 class NeuralNetValueFunction(object):
-    def __init__(self, ob_dim, ac_dim): #pylint: disable=W0613
+    def __init__(self, ob_dim, ac_dim):
         X = tf.placeholder(tf.float32, shape=[None, ob_dim*2+ac_dim*2+2]) # batch of observations
         vtarg_n = tf.placeholder(tf.float32, shape=[None], name='vtarg')
 
