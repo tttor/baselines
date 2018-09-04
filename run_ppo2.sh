10d3528e77de102da937a9784b02a2ee16ac3f5e
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-09-04 13:30:26 +1000

    add run ppo sh

---
+++ run_ppo2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "USAGE:"
+  echo "bash run.sh <env_id>"
+  exit 1
+fi
+env_id=${1}
+
+python -m baselines.run
+--env=$env_id
+--seed=12
+--alg=ppo2
+--num_timesteps=1e6
+--network=mlp
+--num_env=1
+--reward_scale=1.0
