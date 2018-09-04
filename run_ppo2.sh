8006ef88088541cf902a095891876eda0f7afb7d
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-09-04 13:43:55 +1000

    add run ppo sh 2

--- run_ppo2.sh
+++ run_ppo2.sh
@@ -6,11 +6,11 @@ if [ "$#" -ne 1 ]; then
 fi
 env_id=${1}
 
-python -m baselines.run
---env=$env_id
---seed=12
---alg=ppo2
---num_timesteps=1e6
---network=mlp
---num_env=1
+python -m baselines.run \
+--env=$env_id \
+--seed=12 \
+--alg=ppo2 \
+--num_timesteps=1e6 \
+--network=mlp \
+--num_env=1 \
 --reward_scale=1.0
